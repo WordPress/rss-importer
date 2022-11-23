5fe443685374b90af6db9b546cac6608cfa65630
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2022-11-23 05:46:51 +0100

    Fix: code style.

--- rss-importer.php
+++ rss-importer.php
@@ -48,7 +48,7 @@ class RSS_Import extends WP_Importer {
 	function header() {
 		echo '<div class="wrap">';
 
-		if ( version_compare(get_bloginfo('version'), '3.8.0', '<') ) {
+		if ( version_compare( get_bloginfo( 'version' ), '3.8.0', '<' ) ) {
 			screen_icon();
 		}
 
